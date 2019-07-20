baac6f99a6019dcdcfa6a84b02f0be3d91949a7f
NICHOLAS DEWAAL <nicholasdewaal@MacBook-Pro.local>
2019-07-20 10:19:27 +0900

    fixed type for heroku commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const   express     = require('express'),
         forecast    = require('./utils/forecast');
 
 const app = express();
-const port =
+
 
 //define paths for Express Config
 const publicDirectoryPath = path.join(__dirname, '../public');
