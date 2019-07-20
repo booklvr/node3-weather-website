703086f12b5e242a5d2faab6e496d5ceb2f247dd
NICHOLAS DEWAAL <nicholasdewaal@MacBook-Pro.local>
2019-07-20 10:12:47 +0900

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-
-
-fetch('http://localhost:8080/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    });
-});
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -26,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = '';
 
 
-    fetch(`http://localhost:8080/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error);
